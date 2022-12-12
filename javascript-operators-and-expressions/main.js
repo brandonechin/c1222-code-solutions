efb985a34356e0027a7d93caf4862700641114b4
Brandon Chin <bbrandonchin@yahoo.com>
2022-12-12 19:34:48 +0000

    Completed javascript-operators-and-expressions exercise

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,43 @@
+var width = 5;
+var height = 7;
+
+var area = width * height;
+console.log('Value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 20;
+var payment = 30;
+
+var change = payment - bill;
+console.log('Value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 15;
+var midterm = 40;
+var final = 50;
+
+var grade = ((quizzes + midterm + final) / 3);
+console.log('Value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Brandon';
+var lastName = 'Chin';
+
+var fullName = firstName + ' ' + lastName;
+console.log('Value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 35;
+var isAcidic = pH < 7;
+console.log('Value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 55;
+var isSparta = headCount === 300;
+console.log('Value of isSparta', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('Value of motto:', motto);
+console.log('typeof motto:', typeof motto);
