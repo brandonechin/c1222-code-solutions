7d7f9c054b29c16c11028f3c32a487db2e1fcc7f
Brandon Chin <bbrandonchin@yahoo.com>
2022-12-13 18:58:07 +0000

    Completed javascript-data-model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,80 @@
+var orderHistory = [
+  {
+    orderPlaced: 'August 4, 2020',
+    total: '$34.00',
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    deliveryDate: 'August 8, 2020',
+    deliveryDetails: null,
+    image: '',
+    product: 'Book',
+    title: 'JavaScript for imaptient programmers',
+    author: 'Rauschmayer Dr. Axel',
+    returnClosed: 'September 7, 2020',
+    subTotal: '$31.55',
+    items: '1'
+  },
+  {
+    orderPlaced: 'July 19, 2020',
+    total: '$44.53',
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    deliveryDate: 'July 20, 2020',
+    deliveryDetails: 'Your package was delivered. It was handed directly to a resident.',
+    image: '',
+    product: 'Book',
+    title: 'The Timeless Way of Building',
+    author: 'Alexander Christopher',
+    returnClosed: 'August 19, 2020',
+    subTotal: '$41.33',
+    items: '1'
+  },
+  {
+    orderPlaced: 'July 4, 2020',
+    total: '$17.22',
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    deliveryDate: 'July 7,2020',
+    deliveryDetails: 'Your package was delivered. It was handed directly to a resident.',
+    image: '',
+    product: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No-Lag-Gamecube Adapter',
+    title: null,
+    author: null,
+    returnClosed: 'August 5, 2020',
+    subTotal: '$15.98',
+    items: '1'
+  },
+  {
+    items: [
+      {
+        orderPlaced: 'July 3, 2020',
+        total: '$138.93',
+        shipTo: 'JS Masher',
+        orderNumber: '113-2883177-2648248',
+        deliveryDate: 'July 5, 2020',
+        deliveryDetails: null,
+        image: '',
+        product: 'GameCube Controll - Super Smash Bros. Edition (Nintendo Switch)',
+        title: null,
+        author: null,
+        returnClosed: 'August 4, 2020',
+        subTotal: '$94.95'
+      },
+      {
+        orderPlaced: 'July 3, 2020',
+        total: '$138.93',
+        shipTo: 'JS Masher',
+        orderNumber: '113-2883177-2648248',
+        deliveryDate: 'July 5, 2020',
+        deliveryDetails: null,
+        image: '',
+        product: 'book',
+        title: 'The Art of Sql',
+        author: 'Faroult Stephane',
+        returnClosed: 'August 4, 2020',
+        subTotal: '$33.99'
+      }
+    ]
+  }
+];
+console.log('value of orderHistory:', orderHistory);
