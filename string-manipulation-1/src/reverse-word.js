ec48961ca2042fd540beb6a914d9feefb55491f2
Brandon Chin <bbrandonchin@yahoo.com>
2022-12-16 22:45:56 +0000

    updated the pseudo code"

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,5 +1,8 @@
 /* exported reverseWord */
-/* reverse the order of the characters in a string */
+/* reverse the order of the characters in a string.
+create a loop that starts with the last index first. Assign the letter in that last index to a new string.
+keep doing that until there are no more letters in the word.
+*/
 function reverseWord(word) {
   var newString = '';
   for (let i = word.length - 1; i >= 0; i--) {
