0fe9ac54f7842d2a72bf27cd18aadd7fc65b5b5c
Brandon Chin <bbrandonchin@yahoo.com>
2023-02-07 20:02:05 +0000

    Completed react-form-controls exercise

--- react-form-controls/src/index.jsx
+++ react-form-controls/src/index.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 export function RegistrationForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleUsernameChange(event) {
+    setUsername(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log('state:', { username, password });
+  }
 
   return (
-    <form>
-      <input></input>
-      <input></input>
-      <button></button>
+    <form onSubmit={handleSubmit}>
+      <label>Username</label>
+      <input type='text' name='username' value={username} onChange={handleUsernameChange} />
+      <label>Password</label>
+      <input type='password' name='password' value={password} onChange={handlePasswordChange} />
+      <button type='submit'>Sign Up</button>
     </form>
   );
 }
-
 const container = document.querySelector('#root');
 
 const root = ReactDOM.createRoot(container);
