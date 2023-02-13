a2a712933d88bdc099eea25fd0b4272568c2682b
Brandon Chin <bbrandonchin@yahoo.com>
2023-02-13 19:39:56 +0000

    added catch method in the event the promise is rejected.

--- fetch/main.js
+++ fetch/main.js
@@ -1,6 +1,9 @@
 fetch('https://jsonplaceholder.typicode.com/users')
   .then((response) => response.json())
-  .then((response) => console.log(response));
+  .then((response) => console.log(response))
+  .catch((error) => console.log(error));
+
 fetch('https://pokeapi.co/api/v2/pokemon/1')
   .then((response) => response.json())
-  .then((response) => console.log(response));
+  .then((response) => console.log(response))
+  .catch((error) => console.log(error));
