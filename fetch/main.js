bac45e2af3e4635a5e61c6eed10519afb50bea9a
Brandon Chin <bbrandonchin@yahoo.com>
2023-02-13 19:13:34 +0000

    Completed fetch exercise.

---
+++ fetch/main.js
@@ -0,0 +1,6 @@
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
+  .then((response) => console.log(response));
+fetch('https://pokeapi.co/api/v2/pokemon/1')
+  .then((response) => response.json())
+  .then((response) => console.log(response));
